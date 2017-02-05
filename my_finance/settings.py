4cec1eda2995573575438ab48619701a3219aeea
kldgarrido <kldgarrido@gmail.com>
2017-02-05 08:56:49 -0500

    dj_database_url

--- my_finance/settings.py
+++ my_finance/settings.py
@@ -78,12 +78,9 @@ WSGI_APPLICATION = 'my_finance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url   ####not working for my case
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 AUTH_PASSWORD_VALIDATORS = [
     {
