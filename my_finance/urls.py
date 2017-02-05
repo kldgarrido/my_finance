1c4fb1efd7270d9cbbcd8a2f92e2e83ce47b7c09
kldgarrido <kldgarrido@gmail.com>
2017-02-05 10:26:09 -0500

    add expense operation

--- my_finance/urls.py
+++ my_finance/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^expense_operation_add/', login_required(expense_operation)),
 ]
---
+++ my_finance/views.py
@@ -0,0 +1,29 @@
+from .models import Expense, ExpenseOperation
+from uuid import uuid4
+from django.shortcuts import render
+import datetime
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+
+
+login_required
+def expense_operation(request):
+    id = str(uuid4())
+    descriptionIn = request.GET.get('inputDescription', '')
+
+    if(not descriptionIn == ""):
+        balanceIn = float(request.GET.get('inputBalance', 0))
+
+        account = Expense.objects.get(id=request.GET.get('inputAccount', ''))
+        expense_op = ExpenseOperation(id, description=account.name, expense=account, balance=balanceIn,
+                                      operation_date=datetime.datetime.now())
+        expense_op.save()
+
+    expenses = Expense.objects.all()
+    current_date = datetime.datetime.now()
+    return render_to_response('expense_operation.html', {'accounts': expenses, 'now': current_date})
+
+# </Expense>
+
+def expense(request):
+    return render(request, 'myapp/index.html')
